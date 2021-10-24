265c47509de6df7d08d700926b49289e775e8749
asish <asish@accubits.com>
2021-10-24 13:31:54 +0530

    alembic revision

---
+++ alembic/versions/59e0a9d2c469_initial.py
@@ -0,0 +1,24 @@
+"""Initial
+
+Revision ID: 59e0a9d2c469
+Revises: 
+Create Date: 2021-10-24 13:29:58.696032
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '59e0a9d2c469'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
